586f99bf2be1a7825b7d4e346118343d14db1a8e
techwizard31 <istaprasad.patra@gmail.com>
2024-12-14 13:06:51 +0530

    navbar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import Events from "@/components/Events";
+import Navbar from "../components/Navbar"
 export default function Home() {
   return (
    <>
+   <Navbar />
    <Events/>
    </>
   );
