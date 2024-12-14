ded81a6c999d5cc2b901039d3b8c06a82137a7de
Debajyoti <chatterjeedebajyoti254@gmail.com>
2024-12-14 22:52:50 +0530

    footer section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Events from "@/components/Events";
-import Navbar from "../components/Navbar"
+import Navbar from "../components/Navbar";
+import Footer from "@/components/footer";
 export default function Home() {
   return (
-   <>
-   <Navbar />
-   <Events/>
-   </>
+    <>
+      <Navbar />
+      <Events />
+      <Footer />
+    </>
   );
 }
