459d93ea5d226a689f0b60fcf2bd69f7f9b33232
shriyaaditya <shriyadalai6@gmail.com>
2024-12-14 18:03:10 +0530

    added hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Events from "@/components/Events";
 import Navbar from "../components/Navbar"
+import Hero from "../components/Hero"
 export default function Home() {
   return (
    <>
    <Navbar />
+   <Hero />
    <Events/>
    </>
   );
