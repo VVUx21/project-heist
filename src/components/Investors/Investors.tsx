6080f7c0f63edda13dbe7a1996360d5ee33b1252
narashimha05 <chinnarinarashimha@gmail.com>
2024-12-17 12:43:06 +0530

    refactor: Add change in speakers, sponsors and investors

--- src/components/Investors/Investors.tsx
+++ src/components/Investors/Investors.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Investors.css"
 import Image from 'next/image'
 import { investorsData } from "../../Data/InvestorsData"
+import { Carousel } from "flowbite-react";
 const Investors = () => {
   return (
     <div id='Investors'>
@@ -9,8 +10,8 @@ const Investors = () => {
             <p>PAST <span className='text-black gradient pt-[7.5px] pl-2 pr-2 '>INVESTORS</span></p>
         </div>
         <div className="w-full">
-    <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-4 flex flex-col justify-center items-center">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-3/4 ">
+    <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 flex flex-col justify-center items-center">
+        <div className="hidden sm:grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-3/4 ">
             {investorsData.map((team, index) => (
                 <div key={team.id} className="w-full overflow-hidden flex flex-col justify-center items-center">
                     <div>
@@ -27,6 +28,18 @@ const Investors = () => {
             ))}
         </div>
     </section>
+
+     <div className="sm:hidden h-56 sm:h-64 xl:h-80 2xl:h-96 ml-8 mr-6 mb-4">
+          <Carousel slideInterval={1000} indicators={false}>
+            <Image src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" alt="..." layout='fixed' height={500} width={500}/>
+            <Image src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" alt="..." layout='fixed' height={500} width={500}/>
+            <Image src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" alt="..." layout='fixed' height={500} width={500}/>
+            <Image src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" alt="..." layout='fixed' height={500} width={500}/>
+            <Image src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" alt="..." layout='fixed' height={500} width={500}/>
+            <Image src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" alt="..." layout='fixed' height={500} width={500}/>
+            {/* <img src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" alt="..." />*/}
+          </Carousel>
+        </div>
 </div>
     </div>
   )
--- src/components/Sponsors/Sponsors.tsx
+++ src/components/Sponsors/Sponsors.tsx
@@ -11,7 +11,7 @@ function Sponsors() {
         </div>
 
 <main className="relative h-full flex flex-col justify-center overflow-hidden">
-    <div className="w-full max-w-5xl mx-auto px-4 md:px-6 py-24">
+    <div className="w-full max-w-5xl mx-auto px-4 md:px-6 py-16">
         <div className="text-center">
 
             
@@ -42,7 +42,7 @@ function Sponsors() {
                     ul.insertAdjacentHTML('afterend', ul.outerHTML);
                     ul.nextSibling.setAttribute('aria-hidden', 'true');
                 })"
-                className="w-full mt-[3rem] inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]"
+                className="w-full mt-[3rem] sm:inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)] hidden"
             >
                 <ul x-ref="logos" className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll-rtl ">
                   { sponsorData.map((item: any) => (
