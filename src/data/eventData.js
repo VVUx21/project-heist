ba041b64a7887c6fc9bffa214760d01707733234
Sujal Kumar Agarwal <sujalkumaragarwal911@Developer-Kit.local>
2025-01-03 22:42:55 +0530

    event data changed

--- src/data/eventData.js
+++ src/data/eventData.js
@@ -1,20 +1,50 @@
 export const eventsData = [
   {
     "id": 1,
-    "title": "EVENT 1",
+    "title": "Shark Tank",
     "image": "https://res.cloudinary.com/dnw1mcx2h/image/upload/v1734109550/ouyss7092bsvsvlvrkan.png",
-    "text": "Te cultivate secretary acheer trick tire prejudice declare elect search explosive right rank pair steam compare perhaps ring pass bare example wi"
+    "text": "Pitch innovative ideas to top investors for funding and mentorship. Witness startups turning dreams into reality at NES 2025's most awaited event."
   },
   {
     "id": 2,
-    "title": "EVENT 2",
+    "title": "Startup Expo",
     "image": "https://res.cloudinary.com/dnw1mcx2h/image/upload/v1734109550/ouyss7092bsvsvlvrkan.png",
-     "text": "Te cultivate secretary acheer trick tire prejudice declare elect search explosive right rank pair steam compare perhaps ring pass bare example wi"
+     "text": "Discover groundbreaking products and solutions while startups connect with investors, mentors, and customers for growth opportunities."
   },
   {
     "id": 3,
-    "title": "EVENT 3",
+    "title": "Virtual Trading",
     "image": "https://res.cloudinary.com/dnw1mcx2h/image/upload/v1734109550/ouyss7092bsvsvlvrkan.png",
-     "text": "Te cultivate secretary acheer trick tire prejudice declare elect search explosive right rank pair steam compare perhaps ring pass bare example wi"
+    "text": "Experience the excitement of stock market simulations. Trade virtually, analyze trends, and demonstrate your financial expertise."
+  },
+  {
+    "id": 4,
+    "title": "IPL Auction",
+    "image": "https://res.cloudinary.com/dnw1mcx2h/image/upload/v1734109550/ouyss7092bsvsvlvrkan.png",
+     "text": " Showcase your strategy in a thrilling fantasy cricket auction. Bid, plan, and win with your ultimate team-building skills."
+  },
+  {
+    "id": 5,
+    "title": "Workshops",
+    "image": "https://res.cloudinary.com/dnw1mcx2h/image/upload/v1734109550/ouyss7092bsvsvlvrkan.png",
+    "text": "Learn from industry experts in interactive sessions covering entrepreneurship, technology, and innovation to sharpen your skills."
+  },
+  {
+    "id": 6,
+    "title": "Competitions",
+    "image": "https://res.cloudinary.com/dnw1mcx2h/image/upload/v1734109550/ouyss7092bsvsvlvrkan.png",
+     "text": "Test your creativity and problem-solving abilities in exciting challenges, with prizes and networking opportunities up for grabs."
+  },
+  {
+    "id": 7,
+    "title": "Guest Lectures",
+    "image": "https://res.cloudinary.com/dnw1mcx2h/image/upload/v1734109550/ouyss7092bsvsvlvrkan.png",
+     "text": "Hear from global leaders and innovators as they share insights on entrepreneurship, technology, and career growth."
+  },
+  {
+    "id": 8,
+    "title": "Business Hackathon",
+    "image": "https://res.cloudinary.com/dnw1mcx2h/image/upload/v1734109550/ouyss7092bsvsvlvrkan.png",
+     "text": " A 24-hour innovation marathon solving real-world challenges. Compete for prizes and industry recognition with creative solutions"
   }
 ]
