41a0dfe10bbcd3ca24d208089c28620d714672cc
techwizard31 <istaprasad.patra@gmail.com>
2025-01-17 23:00:39 +0530

    trying

--- src/app/(auth)/verify/[uniquecode]/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import RegisterForm from "@/components/Authform";
-import { useParams } from 'next/navigation';
-function Verify() {
-  const params = useParams<{ uniquecode: string }>();
-  return <RegisterForm type="verify" email={params.uniquecode} />;
-}
-
-export default Verify;
